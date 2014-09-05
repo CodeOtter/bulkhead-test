32cd1ec0bb408a95fd6682777f8521b53746bd65
12dCode <12dCode@gmail.com>
2014-09-05 12:53:15 -0700

    Adding bulkhead plugin initializer

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Sails = require('sails'),
-Barrels = require('barrels'),
-supertest = require('supertest'),
-fs = require('fs');
+	Barrels = require('barrels'),
+	supertest = require('supertest'),
+	fs = require('fs'),
+	Bulkhead = require('bulkhead');
 
 module.exports = {
 
@@ -48,30 +49,44 @@ module.exports = {
 						sails: true,
 						services: true,
 						models: true
+					},
+					models: {
+						migrate: 'safe'
 					}
 				}, function(err, sails) {
-					self.singleton = sails;
+					Bulkhead.plugins.initialize(sails, function() {
+						// Initialize all plugins
+						self.singleton = sails;
 
-					if(fs.existsSync(process.cwd() + '/test/fixtures')) {
-						// Fixtures exist
-						Barrels.load();	
-					}
+						if(fs.existsSync(process.cwd() + '/test/fixtures')) {
+							// Fixtures exist
+							Barrels.load();	
+						}
 
-					if(Object.keys(Barrels.objects).length > 0) {
-						// Put in a placeholder just in case nothing loads
-						Barrels.populate(function(barrelErr) {
-							// Populate the DB
-							if(barrelErr) {
-								console.log(barrelErr);
-								throw barrelErr;
-							}
+						if(Object.keys(Barrels.objects).length > 0) {
+							// Put in a placeholder just in case nothing loads
+							Barrels.populate(function(barrelErr) {
+								// Populate the DB
+								if(barrelErr) {
+									console.log(barrelErr);
+									throw barrelErr;
+								}
 
-							// Clear the test redis
-							//QueueService.queue('test').client.flushdb(function() {
-							//	done(err, sails);
-							//});
-							self.fixtures = Barrels.objects;
+								// Clear the test redis
+								//QueueService.queue('test').client.flushdb(function() {
+								//	done(err, sails);
+								//});
+								self.fixtures = Barrels.objects;
 
+								if(beforeCb) {
+									beforeCb(sails, function(err) {
+										done(err, sails);
+									});
+								} else {
+									done(null, sails);
+								}
+							});
+						} else {
 							if(beforeCb) {
 								beforeCb(sails, function(err) {
 									done(err, sails);
@@ -79,16 +94,8 @@ module.exports = {
 							} else {
 								done(null, sails);
 							}
-						});
-					} else {
-						if(beforeCb) {
-							beforeCb(sails, function(err) {
-								done(err, sails);
-							});
-						} else {
-							done(null, sails);
 						}
-					}
+					});
 				});	
 			});
 			
